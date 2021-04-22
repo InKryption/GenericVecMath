dfceb3a82b05fbabedbfbf35a548baa4f98cd968
InK <InKryption07@gmail.com>
2021-04-22 16:16:36 +0200

    Clean a couple things

--- src/MathVector.hpp
+++ src/MathVector.hpp
@@ -85,268 +85,6 @@ namespace ink {
 		
 	}
 	
-	namespace attempt2 {
-		
-		template<typename T, size_t axis_index>
-		struct Axis;
-		
-		template<size_t axis_index>
-		struct Axis<void, axis_index>
-		{
-			static constexpr bool
-			is_void = true;
-			
-			constexpr auto&
-			data() { return *this; }
-			
-			constexpr
-			Axis(std::nullptr_t = nullptr)
-			noexcept {}
-		};
-		
-		template<typename T> requires(!std::same_as<T, void>)
-		struct Axis<T, 0>
-		{
-			static constexpr bool
-			is_void = false;
-			
-			constexpr auto&
-			data() { return x; }
-			
-			constexpr
-			Axis()
-			noexcept( noexcept(T()) ):
-			x() {}
-			
-			constexpr
-			Axis(auto&& x)
-			noexcept( noexcept(T( std::declval<T>() )) ):
-			x(x) {}
-			
-			T x;
-		};
-		
-		template<typename T> requires(!std::same_as<T, void>)
-		struct Axis<T, 1>
-		{
-			static constexpr bool
-			is_void = false;
-			
-			constexpr auto&
-			data() { return y; }
-			
-			constexpr
-			Axis()
-			noexcept( noexcept(T()) ):
-			y() {}
-			
-			constexpr
-			Axis(auto&& y)
-			noexcept( noexcept(T( std::declval<T>() )) ):
-			y(y) {}
-			
-			T y;
-			
-		};
-		
-		template<typename T> requires(!std::same_as<T, void>)
-		struct Axis<T, 2>
-		{
-			static constexpr bool
-			is_void = false;
-			
-			constexpr auto&
-			data() { return z; }
-			
-			constexpr
-			Axis()
-			noexcept( noexcept(T()) ):
-			z() {}
-			
-			constexpr
-			Axis(auto&& z)
-			noexcept( noexcept(T( std::declval<T>() )) ):
-			z(z) {}
-			
-			T z;
-			
-		};
-		
-		
-		
-		template<typename Axis1, typename Axis2, typename Axis3>
-		struct VecBase;
-		
-		template<
-			typename T_A1, size_t I_A1,
-			typename T_A2, size_t I_A2,
-			typename T_A3, size_t I_A3
-		> struct VecBase<
-			Axis<T_A1, I_A1>,
-			Axis<T_A2, I_A2>,
-			Axis<T_A3, I_A3>
-		>: public Axis<T_A1, I_A1>, Axis<T_A2, I_A2>, Axis<T_A3, I_A3>
-		{
-			
-			private: using
-			A1 = Axis<T_A1, I_A1>;
-			private: using
-			A2 = Axis<T_A2, I_A2>;
-			private: using
-			A3 = Axis<T_A3, I_A3>;
-			
-			private: template<size_t Idx> static constexpr auto&&
-			forward_param(auto&& x, auto&& y, auto&& z) noexcept {
-				if constexpr(Idx == 0) return x;
-				if constexpr(Idx == 1) return y;
-				if constexpr(Idx == 2) return z;
-			}
-			
-			public: constexpr
-			VecBase()
-				noexcept(noexcept(A1())					&& noexcept(A2())					&& noexcept(A3())					)
-				requires(std::default_initializable<A1>	&& std::default_initializable<A2>	&& std::default_initializable<A3>	):
-				A1(), A2(), A3()
-			{}
-			
-			public: constexpr
-			VecBase(auto&& x, auto&& y, auto&& z)
-				noexcept(noexcept( A1(std::declval<A1>()) )	&&	noexcept( A2(std::declval<A2>()) )	&&	noexcept( A3(std::declval<A3>() ))):
-				A1(forward_param<I_A1>(x, y, z)),
-				A2(forward_param<I_A2>(x, y, z)),
-				A3(forward_param<I_A3>(x, y, z))
-			{}
-			
-		};
-		
-		template<typename X, typename Y, typename Z>
-		using PackedVecBase
-		=	typename internal
-		::	AlignTypes_t<	Axis<X,0> , Axis<Y,1> , Axis<Z,2>	>
-		::	rebind< VecBase >;
-		
-		template<typename X, typename Y = X, typename Z = Y>
-		class Vec: public PackedVecBase<X, Y, Z>
-		{
-			template<typename,typename,typename> friend class Vec;
-			
-			private:
-			using base = PackedVecBase<X, Y, Z>;
-			using AxisX = Axis<X, 0>;
-			using AxisY = Axis<Y, 1>;
-			using AxisZ = Axis<Z, 2>;
-			
-			/* CONSTRUCTORS */
-			
-			public: constexpr
-			Vec() noexcept(noexcept(base()))
-			requires(std::default_initializable<base>):
-			base() {}
-			
-			public: constexpr
-			Vec(std::convertible_to<X> auto&& x,
-				std::convertible_to<Y> auto&& y,
-				std::convertible_to<Z> auto&& z)
-				noexcept( noexcept(base( std::declval<X>() , std::declval<Y>() , std::declval<Z>() )) )
-				requires(!AxisX::is_void && !AxisY::is_void && !AxisZ::is_void):
-				base(x, y, z)
-			{}
-			
-			public: constexpr
-			Vec(std::convertible_to<X> auto&& x,
-				std::convertible_to<Y> auto&& y)
-				noexcept( noexcept(base( std::declval<X>() , std::declval<Y>() , nullptr )) )
-				requires(!AxisX::is_void && !AxisY::is_void && AxisZ::is_void):
-				base(x, y, nullptr)
-			{}
-			
-			public: constexpr
-			Vec(std::convertible_to<X> auto&& x,
-				std::convertible_to<Z> auto&& z)
-				noexcept( noexcept(base( std::declval<X>() , nullptr , std::declval<Z>() )) )
-				requires(!AxisX::is_void && AxisY::is_void && !AxisZ::is_void):
-				base(x, nullptr, z)
-			{}
-			
-			public: constexpr
-			Vec(std::convertible_to<Y> auto&& y,
-				std::convertible_to<Z> auto&& z)
-				noexcept( noexcept(base( nullptr , std::declval<Y>() , std::declval<Z>() )) )
-				requires(AxisX::is_void && !AxisY::is_void && !AxisZ::is_void):
-				base(nullptr, y, z)
-			{}
-			
-			public: constexpr
-			Vec(std::convertible_to<X> auto&& x)
-				noexcept( noexcept(base( std::declval<X>() , nullptr , nullptr )) )
-				requires(!AxisX::is_void && AxisY::is_void && AxisZ::is_void):
-				base(x, nullptr, nullptr)
-			{}
-			
-			public: constexpr
-			Vec(std::convertible_to<Y> auto&& y)
-				noexcept( noexcept(base( nullptr , std::declval<Y>() , nullptr )) )
-				requires(AxisX::is_void && !AxisY::is_void && AxisZ::is_void):
-				base(nullptr, y, nullptr)
-			{}
-			
-			public: constexpr
-			Vec(std::convertible_to<Z> auto&& z)
-				noexcept( noexcept(base( nullptr , nullptr , std::declval<Z>() )) )
-				requires(AxisX::is_void && AxisY::is_void && !AxisZ::is_void):
-				base(nullptr, nullptr, z)
-			{}
-			
-			/* GET */
-			
-			public: template<size_t Idx> friend constexpr decltype(auto)
-			get(Vec & v) noexcept {
-				if constexpr(Idx == 0 && !AxisX::is_void) return static_cast<AxisX*>(&v)->data();
-				if constexpr(Idx == 1 && !AxisY::is_void) return static_cast<AxisY*>(&v)->data();
-				if constexpr(Idx == 2 && !AxisZ::is_void) return static_cast<AxisZ*>(&v)->data();
-			}
-			
-			public: template<size_t Idx> friend constexpr decltype(auto)
-			get(Vec && v) noexcept
-			{ return std::move(get<Idx>(v)); }
-			
-			public: template<size_t Idx> friend constexpr auto const&
-			get(Vec const& v) noexcept
-			{ return get<Idx>(const_cast<Vec&>(v)); }
-			
-			/* OPERATORS */
-			
-			private: static constexpr auto
-			OP_add		= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs + rhs)) { return lhs + rhs; },
-			OP_sub		= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs - rhs)) { return lhs - rhs; },
-			OP_mul		= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs * rhs)) { return lhs * rhs; },
-			OP_div		= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs / rhs)) { return lhs / rhs; },
-			OP_mod		= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs % rhs)) { return lhs % rhs; },
-			OP_bitand	= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs & rhs)) { return lhs & rhs; },
-			OP_bitor	= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs | rhs)) { return lhs | rhs; },
-			OP_xor		= [](auto&& lhs, auto&& rhs) constexpr noexcept(noexcept(lhs ^ rhs)) { return lhs ^ rhs; };
-			
-			private: template<typename RX, typename RY, typename RZ>
-			struct BinaryArithmeticFunc {
-				using VecRHS = Vec<RX, RY, RZ>;
-				template<auto OP, typename L, typename R>
-				using Res = decltype( OP(std::declval<L>(), std::declval<R>()) );
-				
-				template<auto OP> static constexpr decltype(auto)
-				operate(Vec const& lhs, VecRHS const& rhs)
-				noexcept( noexcept(OP(lhs.x, rhs.x)) && noexcept(OP(lhs.y, rhs.y)) && noexcept(lhs.z, rhs.z) )
-				{
-					return Vec
-					<Res<OP, X, RX>, Res<OP, Y, RY>, Res<OP, Z, RZ>>
-					(OP(lhs.x, rhs.x), OP(lhs.y, rhs.y), OP(lhs.z, rhs.z));
-				}
-				
-			};
-			
-		};
-		
-	}
-	
 	namespace detail {
 		
 		enum class AxisTag { x , y , z };
@@ -418,10 +156,10 @@ namespace ink {
 			
 		};
 		
+		// Empty / Void axis; empty value.
 		template<AxisTag tag>
 		class Axis<void, tag> {
 			public:
-			constexpr Axis(auto&&) noexcept {}
 			constexpr Axis(std::nullptr_t = nullptr) noexcept {}
 			
 			protected:
@@ -441,7 +179,7 @@ namespace ink {
 		template<typename T1, AxisTag t1, typename T2, AxisTag t2, typename T3, AxisTag t3>
 		struct Axies
 				<	Axis<T1, t1> , Axis<T2, t2> , Axis<T3, t3>	>:
-		public	Axis<T1, t1> , Axis<T2, t2> , Axis<T3, t3>
+		public		Axis<T1, t1> , Axis<T2, t2> , Axis<T3, t3>
 		{
 			
 			protected:
@@ -567,13 +305,6 @@ namespace ink {
 		
 		template<typename X, typename Y = X, typename Z = Y>
 		using Vec = internal::AlignTypes_t<Axis<X, AxisTag::x>, Axis<Y, AxisTag::y>, Axis<Z, AxisTag::z>>::template rebind<Axies>;
-		
-		void test() {
-			
-			
-			
-			
-		}
 		
 	}
 	
