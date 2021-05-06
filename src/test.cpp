59580e887600418a8007279edcb18814bb9614c0
InK <InKryption07@gmail.com>
2021-05-06 21:26:05 +0200

    indentation/format

--- src/test.cpp
+++ src/test.cpp
@@ -129,46 +129,54 @@ namespace ink {
 		public: template<typename OX, typename OY> constexpr
 		Vec(OX&& vx, OY&& vy)
 		noexcept(noexcept(base(std::declval<OX>(), std::declval<OY>(), nullptr)))
-		requires(std::constructible_from<base, OX, OY, std::nullptr_t>
-		&& std::default_initializable<typename base::baseZ>)
+		requires(
+			std::constructible_from<base, OX, OY, std::nullptr_t>
+		&&	std::default_initializable<typename base::baseZ>)
 		: base(std::forward<OX>(vx), std::forward<OY>(vy), nullptr) {}
-		
-		public: template<typename OY, typename OZ> constexpr
-		Vec(OY&& vy, OZ&& vz)
-		noexcept(noexcept(base(nullptr, std::declval<OY>(), std::declval<OZ>())))
-		requires(std::constructible_from<base, std::nullptr_t, OY, OZ>
-		&& std::is_void_v<X>)
-		: base(nullptr, std::forward<OY>(vy), std::forward<OZ>(vz)) {}
 		
 		public: template<typename OX, typename OZ> constexpr
 		Vec(OX&& vx, OZ&& vz)
 		noexcept(noexcept(base(std::declval<OX>(), nullptr, std::declval<OZ>())))
-		requires(std::constructible_from<base, OX, std::nullptr_t, OZ>
-		&& std::is_void_v<Y>)
+		requires(
+			std::constructible_from<base, OX, std::nullptr_t, OZ>
+		&&	std::is_void_v<Y>)
 		: base(std::forward<OX>(vx), nullptr, std::forward<OZ>(vz)) {}
+		
+		public: template<typename OY, typename OZ> constexpr
+		Vec(OY&& vy, OZ&& vz)
+		noexcept(noexcept(base(nullptr, std::declval<OY>(), std::declval<OZ>())))
+		requires(
+			std::constructible_from<base, std::nullptr_t, OY, OZ>
+		&&	std::is_void_v<X>)
+		: base(nullptr, std::forward<OY>(vy), std::forward<OZ>(vz)) {}
 		
 		
 		
 		public: template<typename OX> constexpr explicit
 		Vec(OX&& vx)
 		noexcept(noexcept(base(std::declval<OX>(), nullptr, nullptr)))
-		requires(std::constructible_from<base, OX, std::nullptr_t, std::nullptr_t>
-		&& std::default_initializable<typename base::baseY>
-		&& std::default_initializable<typename base::baseZ>)
+		requires(
+			std::constructible_from<base, OX, std::nullptr_t, std::nullptr_t>
+		&&	std::default_initializable<typename base::baseY>
+		&&	std::default_initializable<typename base::baseZ>)
 		: base(std::forward<OX>(vx), nullptr, nullptr) {}
 		
 		public: template<typename OY> constexpr explicit
 		Vec(OY&& vy)
 		noexcept(noexcept(base(nullptr, std::declval<OY>(), nullptr)))
-		requires(std::constructible_from<base, std::nullptr_t, OY, std::nullptr_t>
-		&& std::is_void_v<X> && std::default_initializable<typename base::baseZ>)
+		requires(
+			std::constructible_from<base, std::nullptr_t, OY, std::nullptr_t>
+		&&	std::is_void_v<X>
+		&&	std::default_initializable<typename base::baseZ>)
 		: base(nullptr, std::forward<OY>(vy), nullptr) {}
 		
 		public: template<typename OZ> constexpr explicit
 		Vec(OZ&& vz)
 		noexcept(noexcept(base(nullptr, nullptr, std::declval<OZ>())))
-		requires(std::constructible_from<base, std::nullptr_t, std::nullptr_t, OZ>
-		&& std::is_void_v<X> && std::is_void_v<Y>)
+		requires(
+			std::constructible_from<base, std::nullptr_t, std::nullptr_t, OZ>
+		&&	std::is_void_v<X>
+		&&	std::is_void_v<Y>)
 		: base(nullptr, nullptr, std::forward<OZ>(vz)) {}
 		
 	};
