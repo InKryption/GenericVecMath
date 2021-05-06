bd2123f11594f66001ea0101504c37bbd1fd67e3
InK <InKryption07@gmail.com>
2021-05-06 21:27:11 +0200

    Clang config

---
+++ .make/Clang-Debug.mk
@@ -0,0 +1,15 @@
+COMPILER:=clang++
+EXECUTABLE_SHELL:=
+
+BIN:=bin/Clang
+BUILD:=Debug
+PROJECT_NAME_PREFIX:=
+PROJECT_NAME_POSTFIX:=
+ARG:=
+
+CFLAG:=\
+-std=c++20 \
+-D_DEBUG -Og -g \
+-Weverything -Wall -Wextra -Wno-c++98-compat \
+
+LFLAG:=\
