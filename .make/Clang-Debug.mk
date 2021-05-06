e492ac9ea6097225227bb3fa4e8ca0172971fe66
InK <InKryption07@gmail.com>
2021-05-06 23:00:29 +0200

    Change flags

--- .make/Clang-Debug.mk
+++ .make/Clang-Debug.mk
@@ -10,6 +10,6 @@ ARG:=
 CFLAG:=\
 -std=c++20 \
 -D_DEBUG -Og -g \
--Weverything -Wall -Wextra -Wno-c++98-compat \
+-Weverything -Wall -Wextra -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded \
 
 LFLAG:=\
