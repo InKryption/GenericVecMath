597750faa8ef67b444c54a4a7865525a713395fb
InK <InKryption07@gmail.com>
2021-05-07 15:20:14 +0200

    Damn parenthesies

--- src/MathVector.hpp
+++ src/MathVector.hpp
@@ -192,7 +192,7 @@ namespace ink {
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			struct can_shift_right_t:
-			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs >> rhs}; } && !MustBeNoexcept) || requires(Lhs lhs, Rhs rhs) { {lhs >> rhs} noexcept; }> {};
+			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs >> rhs}; } && !MustBeNoexcept) || (requires(Lhs lhs, Rhs rhs) { {lhs >> rhs} noexcept; })> {};
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			concept can_shift_right = can_shift_right_t<Lhs, Rhs, MustBeNoexcept>::value;
@@ -201,7 +201,7 @@ namespace ink {
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			struct can_cmp_threeway_t:
-			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs <=> rhs}; } && !MustBeNoexcept) || requires(Lhs lhs, Rhs rhs) { {lhs <=> rhs} noexcept; }> {};
+			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs <=> rhs}; } && !MustBeNoexcept) || (requires(Lhs lhs, Rhs rhs) { {lhs <=> rhs} noexcept; })> {};
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			concept can_cmp_threeway = can_cmp_threeway_t<Lhs, Rhs, MustBeNoexcept>::value;
@@ -209,7 +209,7 @@ namespace ink {
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			struct can_cmp_less_t:
-			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs < rhs}; } && !MustBeNoexcept) || requires(Lhs lhs, Rhs rhs) { {lhs < rhs} noexcept; }> {};
+			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs < rhs}; } && !MustBeNoexcept) || (requires(Lhs lhs, Rhs rhs) { {lhs < rhs} noexcept; })> {};
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			concept can_cmp_less = can_cmp_less_t<Lhs, Rhs, MustBeNoexcept>::value;
@@ -218,7 +218,7 @@ namespace ink {
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			struct can_cmp_greater_t:
-			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs > rhs}; } && !MustBeNoexcept) || requires(Lhs lhs, Rhs rhs) { {lhs > rhs} noexcept; }> {};
+			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs > rhs}; } && !MustBeNoexcept) || (requires(Lhs lhs, Rhs rhs) { {lhs > rhs} noexcept; })> {};
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			concept can_cmp_greater = can_cmp_greater_t<Lhs, Rhs, MustBeNoexcept>::value;
@@ -227,7 +227,7 @@ namespace ink {
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			struct can_cmp_less_eq_t:
-			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs <= rhs}; } && !MustBeNoexcept) || requires(Lhs lhs, Rhs rhs) { {lhs <= rhs} noexcept; }> {};
+			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs <= rhs}; } && !MustBeNoexcept) || (requires(Lhs lhs, Rhs rhs) { {lhs <= rhs} noexcept; })> {};
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			concept can_cmp_less_eq = can_cmp_less_eq_t<Lhs, Rhs, MustBeNoexcept>::value;
@@ -236,7 +236,7 @@ namespace ink {
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			struct can_cmp_greater_eq_t:
-			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs >= rhs}; } && !MustBeNoexcept) || requires(Lhs lhs, Rhs rhs) { {lhs >= rhs} noexcept; }> {};
+			std::bool_constant<(requires(Lhs lhs, Rhs rhs) { {lhs >= rhs}; } && !MustBeNoexcept) || (requires(Lhs lhs, Rhs rhs) { {lhs >= rhs} noexcept; })> {};
 			
 			template<typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
 			concept can_cmp_greater_eq = can_cmp_greater_eq_t<Lhs, Rhs, MustBeNoexcept>::value;
@@ -309,7 +309,6 @@ namespace ink {
 		
 	}
 	
-	template<typename, typename, typename> class Vec;
 	namespace generic_vec {
 		
 		// Empty struct which acts as a 'null' type in the Vec class, and can be targeted to achieve customized behaviour for other types.
@@ -381,6 +380,22 @@ namespace ink {
 		
 		[[maybe_unused]] static constexpr decltype(auto)
 		operator%(Empty, Empty) noexcept { return Empty(); }
+		
+		
+		
+		[[maybe_unused]] static constexpr decltype(auto)
+		operator<=>(Empty,Empty) noexcept { return std::strong_ordering::equivalent; }
+		
+		[[maybe_unused]] static constexpr decltype(auto)
+		operator==(Empty,auto&& v) noexcept
+		requires(std::is_fundamental_v<std::remove_cvref_t<decltype(v)>>)
+		{ return 0 == v; }
+		
+		[[maybe_unused]] static constexpr decltype(auto)
+		operator==(auto&& v, Empty) noexcept
+		requires(std::is_fundamental_v<std::remove_cvref_t<decltype(v)>>)
+		{ return v == 0; }
+		
 		
 		
 		
@@ -500,88 +515,149 @@ namespace ink {
 		
 	}
 	
-	// Flexible, multi-dimensional, templated math vector class.
-	template<typename X, typename Y = X, typename Z = void>
-	class Vec: public generic_vec::detail::VecBase<X, Y, Z> {
+	namespace generic_vec {
 		
-		private: using base = generic_vec::detail::VecBase<X, Y, Z>;
+		// Flexible, multi-dimensional, templated math vector class.
+		template<typename X, typename Y = X, typename Z = void>
+		class Vec: public generic_vec::detail::VecBase<X, Y, Z> {
+			
+			private: using base = generic_vec::detail::VecBase<X, Y, Z>;
+			
+			public: using value_type_x = decltype(Vec::x);
+			public: using value_type_y = decltype(Vec::y);
+			public: using value_type_z = decltype(Vec::z);
+			
+			public: using Empty = generic_vec::Empty;
+			
+			
+			public: constexpr
+			Vec()
+			noexcept(noexcept(base(nullptr, nullptr, nullptr)))
+			requires(std::constructible_from<base, Empty, Empty, Empty>)
+			: base(nullptr, nullptr, nullptr) {}
+			
+			
+			
+			public: template<typename OX, typename OY, typename OZ>
+			constexpr
+			Vec(Vec<OX, OY, OZ> && other)
+			noexcept(noexcept(base(other.x, other.y, other.z)))
+			requires(std::constructible_from<base, decltype(other.x), decltype(other.y), decltype(other.z)>)
+			: base(static_cast<value_type_x>(other.x), static_cast<value_type_y>(other.y), static_cast<value_type_z>(other.z)) {}
+			
+			public: template<typename OX, typename OY, typename OZ>
+			constexpr
+			Vec(Vec<OX, OY, OZ> const& other)
+			noexcept(noexcept(base(other.x, other.y, other.z)))
+			requires(std::constructible_from<base, decltype(other.x), decltype(other.y), decltype(other.z)>)
+			: base(static_cast<value_type_x>(other.x), static_cast<value_type_y>(other.y), static_cast<value_type_z>(other.z)) {}
+			
+			
+			
+			public: template<typename OX, typename OY, typename OZ>
+			constexpr
+			Vec(OX&& vx, OY&& vy, OZ&& vz)
+			noexcept(noexcept(base(std::declval<OX>(), std::declval<OY>(), std::declval<OZ>())))
+			requires(std::constructible_from<base, OX, OY, OZ>)
+			: base(std::forward<OX>(vx), std::forward<OY>(vy), std::forward<OZ>(vz)) {}
+			
+			
+			
+			public: template<typename OX, typename OY>
+			constexpr
+			Vec(OX&& vx, OY&& vy)
+			noexcept(noexcept(base(std::declval<OX>(), std::declval<OY>(), nullptr)))
+			requires(std::constructible_from<base, OX, OY, Empty> && std::is_void_v<Z>)
+			: base(std::forward<OX>(vx), std::forward<OY>(vy), nullptr) {}
+			
+			public: template<typename OX, typename OZ>
+			constexpr
+			Vec(OX&& vx, OZ&& vz)
+			noexcept(noexcept(base(std::declval<OX>(), nullptr, std::declval<OZ>())))
+			requires(std::constructible_from<base, OX, Empty, OZ> && std::is_void_v<Y>)
+			: base(std::forward<OX>(vx), nullptr, std::forward<OZ>(vz)) {}
+			
+			public: template<typename OY, typename OZ>
+			constexpr
+			Vec(OY&& vy, OZ&& vz)
+			noexcept(noexcept(base(nullptr, std::declval<OY>(), std::declval<OZ>())))
+			requires(std::constructible_from<base, Empty, OY, OZ> && std::is_void_v<X>)
+			: base(nullptr, std::forward<OY>(vy), std::forward<OZ>(vz)) {}
+			
+		};
 		
-		public: using value_type_x = decltype(Vec::x);
-		public: using value_type_y = decltype(Vec::y);
-		public: using value_type_z = decltype(Vec::z);
+		template<typename X, typename Y, typename Z>
+		Vec(X, Y, Z) -> Vec<
+			std::conditional_t<(std::is_null_pointer_v<X> || std::same_as<Empty, X>), void, X>,
+			std::conditional_t<(std::is_null_pointer_v<Y> || std::same_as<Empty, Y>), void, Y>,
+			std::conditional_t<(std::is_null_pointer_v<Z> || std::same_as<Empty, Z>), void, Z>
+		>;
 		
-		public: using Empty = generic_vec::Empty;
+		template<typename X, typename Y>
+		Vec(X, Y) -> Vec<
+			std::conditional_t<(std::is_null_pointer_v<X> || std::same_as<Empty, X>), void, X>,
+			std::conditional_t<(std::is_null_pointer_v<Y> || std::same_as<Empty, Y>), void, Y>, void
+		>;
 		
-		
-		public: constexpr
-		Vec()
-		noexcept(noexcept(base(nullptr, nullptr, nullptr)))
-		requires(std::constructible_from<base, Empty, Empty, Empty>)
-		: base(nullptr, nullptr, nullptr) {}
-		
-		
-		
-		public: template<typename OX, typename OY, typename OZ>
-		constexpr
-		Vec(Vec<OX, OY, OZ> && other)
-		noexcept(noexcept(base(other.x, other.y, other.z)))
-		requires(std::constructible_from<base, decltype(other.x), decltype(other.y), decltype(other.z)>)
-		: base(static_cast<value_type_x>(other.x), static_cast<value_type_y>(other.y), static_cast<value_type_z>(other.z)) {}
-		
-		public: template<typename OX, typename OY, typename OZ>
-		constexpr
-		Vec(Vec<OX, OY, OZ> const& other)
-		noexcept(noexcept(base(other.x, other.y, other.z)))
-		requires(std::constructible_from<base, decltype(other.x), decltype(other.y), decltype(other.z)>)
-		: base(static_cast<value_type_x>(other.x), static_cast<value_type_y>(other.y), static_cast<value_type_z>(other.z)) {}
-		
-		
-		
-		public: template<typename OX, typename OY, typename OZ>
-		constexpr
-		Vec(OX&& vx, OY&& vy, OZ&& vz)
-		noexcept(noexcept(base(std::declval<OX>(), std::declval<OY>(), std::declval<OZ>())))
-		requires(std::constructible_from<base, OX, OY, OZ>)
-		: base(std::forward<OX>(vx), std::forward<OY>(vy), std::forward<OZ>(vz)) {}
-		
-		
-		
-		public: template<typename OX, typename OY>
-		constexpr
-		Vec(OX&& vx, OY&& vy)
-		noexcept(noexcept(base(std::declval<OX>(), std::declval<OY>(), nullptr)))
-		requires(std::constructible_from<base, OX, OY, Empty> && std::is_void_v<Z>)
-		: base(std::forward<OX>(vx), std::forward<OY>(vy), nullptr) {}
-		
-		public: template<typename OX, typename OZ>
-		constexpr
-		Vec(OX&& vx, OZ&& vz)
-		noexcept(noexcept(base(std::declval<OX>(), nullptr, std::declval<OZ>())))
-		requires(std::constructible_from<base, OX, Empty, OZ> && std::is_void_v<Y>)
-		: base(std::forward<OX>(vx), nullptr, std::forward<OZ>(vz)) {}
-		
-		public: template<typename OY, typename OZ>
-		constexpr
-		Vec(OY&& vy, OZ&& vz)
-		noexcept(noexcept(base(nullptr, std::declval<OY>(), std::declval<OZ>())))
-		requires(std::constructible_from<base, Empty, OY, OZ> && std::is_void_v<X>)
-		: base(nullptr, std::forward<OY>(vy), std::forward<OZ>(vz)) {}
-		
-	};
+	}
 	
-	template<typename X, typename Y, typename Z>
-	Vec(X,Y,Z) -> Vec<
-		std::conditional_t<(std::is_null_pointer_v<X> || std::same_as<X, generic_vec::Empty>), void, X>,
-		std::conditional_t<(std::is_null_pointer_v<Y> || std::same_as<Y, generic_vec::Empty>), void, Y>,
-		std::conditional_t<(std::is_null_pointer_v<Z> || std::same_as<Z, generic_vec::Empty>), void, Z>>;
+	using generic_vec::Vec;
 	
-	template<typename X, typename Y>
-	Vec(X,Y) -> Vec<
-		std::conditional_t<(std::is_null_pointer_v<X> || std::same_as<X, generic_vec::Empty>), void, X>,
-		std::conditional_t<(std::is_null_pointer_v<Y> || std::same_as<Y, generic_vec::Empty>), void, Y>, void>;
-	
-	template<typename X, typename Y, typename Z>
-	Vec(Vec<X, Y, Z>) -> Vec<X, Y, Z>;
+	namespace generic_vec {
+		
+		template<typename T> concept IsVec = concepts::same_template<T, Vec<void>>;
+		template<template<typename,typename,bool> typename Constraint, typename Lhs, typename Rhs = Lhs, bool MustBeNoexcept = false>
+		concept Vec_CanDoBinaryOp = IsVec<Lhs> && IsVec<Rhs>
+			&&	Constraint<typename Lhs::value_type_x, typename Lhs::value_type_x, MustBeNoexcept>::value
+			&&	Constraint<typename Lhs::value_type_y, typename Lhs::value_type_y, MustBeNoexcept>::value
+			&&	Constraint<typename Lhs::value_type_z, typename Lhs::value_type_z, MustBeNoexcept>::value;
+		
+		template<typename LX, typename LY, typename LZ, typename RX, typename RY, typename RZ,
+			class LVec = Vec<LX, LY, LZ>,
+			class RVec = Vec<RX, RY, RZ> >
+		requires(Vec_CanDoBinaryOp<concepts::can_add_t, LVec, RVec>)
+		static constexpr decltype(auto)
+		operator+(Vec<LX, LY, LZ> const& lhs, Vec<RX, RY, RZ> const& rhs)
+		noexcept(Vec_CanDoBinaryOp<concepts::can_add_t, LVec, RVec, true>)
+		{ return ink::Vec(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z); }
+		
+		template<typename LX, typename LY, typename LZ, typename RX, typename RY, typename RZ,
+			class LVec = Vec<LX, LY, LZ>,
+			class RVec = Vec<RX, RY, RZ> >
+		requires(Vec_CanDoBinaryOp<concepts::can_sub_t, LVec, RVec>)
+		static constexpr decltype(auto)
+		operator-(Vec<LX, LY, LZ> const& lhs, Vec<RX, RY, RZ> const& rhs)
+		noexcept(Vec_CanDoBinaryOp<concepts::can_sub_t, LVec, RVec, true>)
+		{ return ink::Vec(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z); }
+		
+		template<typename LX, typename LY, typename LZ, typename RX, typename RY, typename RZ,
+			class LVec = Vec<LX, LY, LZ>,
+			class RVec = Vec<RX, RY, RZ> >
+		requires(Vec_CanDoBinaryOp<concepts::can_mul_t, LVec, RVec>)
+		static constexpr decltype(auto)
+		operator*(Vec<LX, LY, LZ> const& lhs, Vec<RX, RY, RZ> const& rhs)
+		noexcept(Vec_CanDoBinaryOp<concepts::can_mul_t, LVec, RVec, true>)
+		{ return ink::Vec(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z); }
+		
+		template<typename LX, typename LY, typename LZ, typename RX, typename RY, typename RZ,
+			class LVec = Vec<LX, LY, LZ>,
+			class RVec = Vec<RX, RY, RZ> >
+		requires(Vec_CanDoBinaryOp<concepts::can_div_t, LVec, RVec>)
+		static constexpr decltype(auto)
+		operator/(Vec<LX, LY, LZ> const& lhs, Vec<RX, RY, RZ> const& rhs)
+		noexcept(Vec_CanDoBinaryOp<concepts::can_div_t, LVec, RVec, true>)
+		{ return ink::Vec(lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z); }
+		
+		template<typename LX, typename LY, typename LZ, typename RX, typename RY, typename RZ,
+			class LVec = Vec<LX, LY, LZ>,
+			class RVec = Vec<RX, RY, RZ> >
+		requires(Vec_CanDoBinaryOp<concepts::can_mod_t, LVec, RVec>)
+		static constexpr decltype(auto)
+		operator%(Vec<LX, LY, LZ> const& lhs, Vec<RX, RY, RZ> const& rhs)
+		noexcept(Vec_CanDoBinaryOp<concepts::can_mod_t, LVec, RVec, true>)
+		{ return ink::Vec(lhs.x % rhs.x, lhs.y % rhs.y, lhs.z % rhs.z); }
+		
+	}
 	
 }
 
--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,12 @@
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[]) {
 	
+	constexpr ink::Vec vec1(1,2,2);
+	constexpr ink::Vec vec2(4,3,nullptr);
+	
+	constexpr auto vec3 = vec2 % vec1;
+	
+	
 	
 	return 0;
 }
