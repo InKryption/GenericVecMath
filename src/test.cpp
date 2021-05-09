40065bf1988766fe44aab8b26c9c9df6c5fa80d4
InK <InKryption07@gmail.com>
2021-05-09 22:55:20 +0200

    For consistency

--- src/test.cpp
+++ src/test.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
+#include <stdio.h>
 #include "MathVector.hpp"
 
 
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char* argv[]) {
 	
+	//auto cross = [](auto&&...) { printf("Hallo!\n"); return 0; };
 	constexpr ink::Vec vx('a');
 	constexpr ink::Vec<void, char> vy('b');
 	constexpr ink::Vec vz(nullptr, nullptr, 'c');
 	constexpr ink::Vec<char, char, char> vxyz
 	= static_cast<decltype(vxyz)>(vx + vy + vz);
+	auto Cross = cross(vx,vy);
 	
 	return 0;
 }
